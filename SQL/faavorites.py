01c73fdca24ad1d7011c120aba651a1ba5980151
Peter Mwansa <mwansapeter800@gmail.com>
2022-10-25 17:17:27 +0300

    Use the dictionary to sort out, arrange in order of most frequent and then reverse  the data

--- SQL/faavorites.py
+++ SQL/faavorites.py
@@ -1,16 +1,23 @@
-import csv 
+import csv
+from pickle import TRUE 
 
-languages = set()
+languages = {}
 
 with open("favourites.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
         language = row["language"].strip().upper()
-        languages.add(language)
+        if language in languages:
+            languages[language] += 1
+        else:
+            languages[language] = 1
 
+# this function will the input and return a corresponding value 
+def get_value(language):
+    return languages[language]
 
-
-for language in languages:
-    print(language)
+# we sort out, then we use the get_value function then we reverse the returned data 
+for language in sorted(languages, key=get_value, reverse=True):
+    print(language, languages[language])
 
 
