5ab07a7e75520d5c889121561fb475c48bb2ed1f
Peter Mwansa <mwansapeter800@gmail.com>
2022-10-25 16:01:17 +0300

    printing only once

--- SQL/faavorites.py
+++ SQL/faavorites.py
@@ -1,8 +1,16 @@
 import csv 
 
+languages = []
+
 with open("favourites.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
-        print(row["language"])
+        if not row["language"] in languages:
+            languages.append(row["language"])
+
+
+
+for language in languages:
+    print(language)
 
 
