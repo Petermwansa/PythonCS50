8accf7df039bbd88b3d702e2e15a9f66789167a5
Peter Mwansa <mwansapeter800@gmail.com>
2022-11-14 17:58:59 +0300

    Write code to interate through all possibilities of a four letter password

--- crack.py
+++ crack.py
@@ -1,7 +1,9 @@
-from string import digits
+from string import digits, ascii_letters
 from itertools import product
 
 
-for passcode in product(digits, repeat=4):
-    print(*passcode)
+# for passcode in product(digits, repeat=4):
+#     print(*passcode)
 
+for passcode in product(ascii_letters, repeat=4):
+    print(*passcode)
